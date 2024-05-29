daea7b5b5764a11d2958de54ede20858f6cf255b
unknown <alhasankhanjibon8090@gmail.com>
2024-05-29 14:10:44 +0600

    project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,293 +1,5 @@
 "use strict";
 
-(function ($) {
-  "use strict";
-
-  /*--------------------------------------------------------------
-  CUSTOM PRE DEFINE FUNCTION
-  ------------------------------------------------------------*/
-  /* is_exist() */
-  jQuery.fn.is_exist = function () {
-    return this.length;
-  };
-  $(function () {
-    // Table BTN Trigger
-    $("#l5-pricing-btn .toggle-btn").on("click", function (e) {
-      console.log($(e.target).parent().parent().hasClass("monthly-active"));
-      $(e.target).toggleClass("clicked");
-      if ($(e.target).parent().parent().hasClass("monthly-active")) {
-        $(e.target).parent().parent().removeClass("monthly-active").addClass("yearly-active");
-      } else {
-        $(e.target).parent().parent().removeClass("yearly-active").addClass("monthly-active");
-      }
-    });
-    $("[data-pricing-trigger]").on("click", function (e) {
-      $(e.target).addClass("active").siblings().removeClass("active");
-      var target = $(e.target).attr("data-target");
-      console.log($(target).attr("data-value-active") == "monthly");
-      if ($(target).attr("data-value-active") == "monthly") {
-        $(target).attr("data-value-active", "yearly");
-      } else {
-        $(target).attr("data-value-active", "monthly");
-      }
-    });
-  }); /*End document ready*/
-
-  $(window).on("resize", function () {}); // end window resize
-
-  $(window).on("load", function () {}); // End window LODE
-
-  /*--------------------------------------------------------------
-  sofax MAP JS
-  ------------------------------------------------------------*/
-  var google_map = $('#map');
-  if (google_map.is_exist()) {
-    var init = function init() {
-      var mapOptions = {
-        zoom: 11,
-        scrollwheel: false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        draggable: true,
-        disableDefaultUI: true,
-        center: new google.maps.LatLng(40.6700, -73.9400),
-        styles: [{
-          "featureType": "landscape.man_made",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#f7f1df"
-          }]
-        }, {
-          "featureType": "landscape.natural",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#d0e3b4"
-          }]
-        }, {
-          "featureType": "landscape.natural.terrain",
-          "elementType": "geometry",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "poi",
-          "elementType": "labels",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "poi.business",
-          "elementType": "all",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "poi.medical",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#fbd3da"
-          }]
-        }, {
-          "featureType": "poi.park",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#bde6ab"
-          }]
-        }, {
-          "featureType": "road",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "road",
-          "elementType": "labels",
-          "stylers": [{
-            "visibility": "off"
-          }]
-        }, {
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#ffe15f"
-          }]
-        }, {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#efd151"
-          }]
-        }, {
-          "featureType": "road.arterial",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#ffffff"
-          }]
-        }, {
-          "featureType": "road.local",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "black"
-          }]
-        }, {
-          "featureType": "transit.station.airport",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#cfb2db"
-          }]
-        }, {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#a2daf2"
-          }]
-        }]
-      };
-      var mapElement = document.getElementById('map');
-      var map = new google.maps.Map(mapElement, mapOptions);
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
-        map: map,
-        // icon: 'assets/images/all-img/contact/map.png',
-        title: 'fugu'
-      });
-      var contentString = '<div id="content">' + '<div id="tpw">' + '<h3>fugu' + '</div>';
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString,
-        maxWidth: 280
-      });
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function () {
-        marker.setAnimation(null);
-      }, 750); //time it takes for one bounce   
-
-      google.maps.event.addListener(marker, 'click', function () {
-        infowindow.open(map, marker);
-      });
-    };
-    google.maps.event.addDomListener(window, 'load', init);
-  }
-
-  //foodko wow js
-  var wow = new WOW({
-    mobile: false,
-    // default
-    tablet: false
-  });
-  wow.init();
-
-  // AOS.init({
-  //   duration: 1200,
-  // })
-})(jQuery);
-
-/*--auto-slider-section--start*/
-
-$('.foodko-text-slider').slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 0,
-  speed: 8000,
-  arrows: false,
-  pauseOnHover: false,
-  cssEase: 'linear',
-  responsive: [{
-    breakpoint: 1199,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }, {
-    breakpoint: 767,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }]
-});
-
-/*-testimonial-slider-2 centermode--start*/
-
-$('.center-slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  centerMode: true,
-  arrows: false,
-  dots: true,
-  speed: 300,
-  centerPadding: '20px',
-  infinite: true,
-  autoplaySpeed: 5000,
-  autoplay: false,
-  responsive: [{
-    breakpoint: 1199,
-    settings: {
-      slidesToShow: 2,
-      slidesToScroll: 1
-    }
-  }, {
-    breakpoint: 767,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }]
-});
-/*-testimonial-slider-2 centermode--end*/
-
-/*--- COUNTER-SLIDER START---*/
-var foodko_counter = $('#foodko-counter');
-if (foodko_counter.is_exist()) {
-  var a = 0;
-  $(window).scroll(function () {
-    var oTop = $(foodko_counter).offset().top - window.innerHeight;
-    if (a == 0 && $(window).scrollTop() > oTop) {
-      $('.foodko-counter').each(function () {
-        var $this = $(this),
-          countTo = $this.attr('data-percentage');
-        $({
-          countNum: $this.text()
-        }).animate({
-          countNum: countTo
-        }, {
-          duration: 4000,
-          easing: 'swing',
-          step: function step() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function complete() {
-            $this.text(this.countNum);
-          }
-        });
-      });
-      a = 1;
-    }
-  });
-}
-/*--- COUNTER-SLIDER END---*/
-
-$('.testimonial-slider-1').slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  autoplay: false,
-  arrows: false,
-  responsive: [{
-    breakpoint: 1199,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }, {
-    breakpoint: 767,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }]
-});
-
 /*----arrow-top-- start-*/
 
 (function ($) {
@@ -295,6 +7,7 @@ $('.testimonial-slider-1').slick({
 
   /*------------------------------------------------------------------
   [Table of contents]
+  
   
   
   -------------------------------------------------------------------*/
@@ -306,118 +19,210 @@ $('.testimonial-slider-1').slick({
   jQuery.fn.is_exist = function () {
     return this.length;
   };
+
+  /* top sticky-menu start*/
   $(function () {
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 50) {
+        $('#sticky-menu').addClass('sticky-menu');
+      } else {
+        $('#sticky-menu').removeClass('sticky-menu');
+      }
+    });
+    /* top sticky-menu end*/
+
+    /*--auto-slider-section--start*/
+
+    $('.foodko-text-slider').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 0,
+      speed: 8000,
+      arrows: false,
+      pauseOnHover: false,
+      cssEase: 'linear',
+      responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    });
+
+    /*-testimonial-slider-2 centermode--start*/
+
+    $('.center-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      arrows: false,
+      dots: true,
+      speed: 300,
+      centerPadding: '20px',
+      infinite: true,
+      autoplaySpeed: 5000,
+      autoplay: false,
+      responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    });
+    /*-testimonial-slider-2 centermode--end*/
+
+    /*--- COUNTER-SLIDER START---*/
+    var foodko_counter = $('#foodko-counter');
+    if (foodko_counter.is_exist()) {
+      var a = 0;
+      $(window).scroll(function () {
+        var oTop = $(foodko_counter).offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+          $('.foodko-counter').each(function () {
+            var $this = $(this),
+              countTo = $this.attr('data-percentage');
+            $({
+              countNum: $this.text()
+            }).animate({
+              countNum: countTo
+            }, {
+              duration: 4000,
+              easing: 'swing',
+              step: function step() {
+                $this.text(Math.floor(this.countNum));
+              },
+              complete: function complete() {
+                $this.text(this.countNum);
+              }
+            });
+          });
+          a = 1;
+        }
+      });
+    }
+    /*--- COUNTER-SLIDER END---*/
+
+    $('.testimonial-slider-1').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: false,
+      arrows: false,
+      responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    });
     /*--------------------------------------------------------------
     STICKY MENU JS INIT
     --------------------------------------------------------------*/
 
-    var progressPath = document.querySelector('.progress-wrap path');
-    var pathLength = progressPath.getTotalLength();
-    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-    progressPath.style.strokeDashoffset = pathLength;
-    progressPath.getBoundingClientRect();
-    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
-    var updateProgress = function updateProgress() {
-      var scroll = $(window).scrollTop();
-      var height = $(document).height() - $(window).height();
-      var progress = pathLength - scroll * pathLength / height;
-      progressPath.style.strokeDashoffset = progress;
-    };
-    updateProgress();
-    $(window).scroll(updateProgress);
-    var offset = 50;
-    var duration = 550;
-    jQuery(window).on('scroll', function () {
-      if (jQuery(this).scrollTop() > offset) {
-        jQuery('.progress-wrap').addClass('active-progress');
-      } else {
-        jQuery('.progress-wrap').removeClass('active-progress');
+    /*--PORTFOLIO-SECTION --ISOTOP START --*/
+
+    // init Isotope
+    var $grid = $('.portfolio-items').isotope({
+      // options
+    });
+    // filter items on menu click
+    $('.portfolio-menu').on('click', 'li', function () {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({
+        filter: filterValue
+      });
+    });
+    $('.portfolio-menu').on('click', 'li', function () {
+      $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    /*-PORTFOLIO-SECTION --ISOTOP END --*/
+
+    /*-testimonial-slider-3-start*/
+    $('.foodko-slider-4').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 300,
+      speed: 300,
+      arrows: false,
+      dots: false,
+      responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    });
+
+    /*-testimonial-slider-3-end*/
+
+    // Get the input fields and the calendar icon
+    var dateInputField = document.getElementById('datepicker');
+    var timeInputField = document.getElementById('timepicker');
+
+    // Function to set current date and time when the input fields are clicked
+    dateInputField.addEventListener('click', function () {
+      this.type = 'date';
+      this.focus();
+    });
+    timeInputField.addEventListener('click', function () {
+      this.type = 'time';
+      this.focus();
+    });
+
+    // If the user manually enters a date or time, change the input type to text
+    dateInputField.addEventListener('change', function () {
+      if (this.value !== '') {
+        this.type = 'text';
       }
     });
-    jQuery('.progress-wrap').on('click', function (event) {
-      event.preventDefault();
-      jQuery('html, body').animate({
-        scrollTop: 0
-      }, duration);
-      return false;
+    timeInputField.addEventListener('change', function () {
+      if (this.value !== '') {
+        this.type = 'text';
+      }
     });
   }); /*End document ready*/
 
   $(window).on("resize", function () {}); // end window resize
 
   $(window).on("load", function () {}); // End window LODE
+
+  //foodko wow js
+  var wow = new WOW({
+    mobile: false,
+    // default
+    tablet: false
+  });
+  wow.init();
 })(jQuery);
 
 /*----arrow-top-- end-*/
-
-/*--PORTFOLIO-SECTION --ISOTOP START --*/
-
-// init Isotope
-var $grid = $('.portfolio-items').isotope({
-  // options
-});
-// filter items on menu click
-$('.portfolio-menu').on('click', 'li', function () {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({
-    filter: filterValue
-  });
-});
-$('.portfolio-menu').on('click', 'li', function () {
-  $(this).addClass('active').siblings().removeClass('active');
-});
-
-/*-PORTFOLIO-SECTION --ISOTOP END --*/
-
-/*-testimonial-slider-3-start*/
-$('.foodko-slider-4').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 300,
-  speed: 300,
-  arrows: false,
-  dots: false,
-  responsive: [{
-    breakpoint: 1199,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }, {
-    breakpoint: 767,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }]
-});
-
-/*-testimonial-slider-3-end*/
-
-// Get the input fields and the calendar icon
-var dateInputField = document.getElementById('datepicker');
-var timeInputField = document.getElementById('timepicker');
-
-// Function to set current date and time when the input fields are clicked
-dateInputField.addEventListener('click', function () {
-  this.type = 'date';
-  this.focus();
-});
-timeInputField.addEventListener('click', function () {
-  this.type = 'time';
-  this.focus();
-});
-
-// If the user manually enters a date or time, change the input type to text
-dateInputField.addEventListener('change', function () {
-  if (this.value !== '') {
-    this.type = 'text';
-  }
-});
-timeInputField.addEventListener('change', function () {
-  if (this.value !== '') {
-    this.type = 'text';
-  }
-});
